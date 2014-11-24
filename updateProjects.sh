730c3562b9f8e85ac20da6862858611c6db22109
Arnaud Héritier <aheritier@exoplatform.com>
2014-11-24 17:29:37 +0100

    set upstream branch when we update a branch

--- updateProjects.sh
+++ updateProjects.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -ue
 
 REMOTE=blessed
-LOCAL_BRANCH=stable/4.0.x
+LOCAL_BRANCH=stable/4.1.x
 REMOTE_BRANCH=$REMOTE/$LOCAL_BRANCH
 #REPLACE_WHAT="<org.gatein.portal.version>3.5.10.Final-SNAPSHOT</org.gatein.portal.version>"
 #REPLACE_BY="<org.gatein.portal.version>3.5.11.Final-SNAPSHOT</org.gatein.portal.version>"
 #COMMIT_MSG="Update Gatein 3.5.10.Final-SNAPSHOT -> 3.5.10.Final-SNAPSHOT"
-REPLACE_WHAT="<org.exoplatform.depmgt.version>7.5</org.exoplatform.depmgt.version>"
-REPLACE_BY="<org.exoplatform.depmgt.version>7.6</org.exoplatform.depmgt.version>"
-COMMIT_MSG="Upgrade maven-depmgt-pom 7.5 -> 7.6"
+REPLACE_WHAT="<org.exoplatform.depmgt.version>9-SNAPSHOT</org.exoplatform.depmgt.version>"
+REPLACE_BY="<org.exoplatform.depmgt.version>9</org.exoplatform.depmgt.version>"
+COMMIT_MSG="Upgrade maven-depmgt-pom 9-SNAPSHOT -> 9"
 #REPLACE_WHAT="<version>13-SNAPSHOT</version>"
 #REPLACE_BY="<version>13</version>"
 #COMMIT_MSG="Use maven-parent-pom 13"
@@ -24,12 +24,13 @@ updateProject (){
   
   git checkout $LOCAL_BRANCH
   git reset --hard $REMOTE_BRANCH
+	git branch --set-upstream-to=$REMOTE/$LOCAL_BRANCH $LOCAL_BRANCH
 
   replaceInPom.sh "$REPLACE_WHAT" "$REPLACE_BY"
   git diff
   pause "Press [Enter] key to continue... We will commit with message : $COMMIT_MSG"
   git commit -m "$COMMIT_MSG" -a || true
-  git push 
+  git push $REMOTE
 
   popd
 }
