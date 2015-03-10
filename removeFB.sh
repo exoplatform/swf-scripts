d94ff877d97c1c8b1892cbca84749e3da3f0ec46
Frederic DROUET <fdrouet@exoplatform.com>
2015-03-10 13:09:02 +0100

    Allow to specify to directory with all the sources repositories

--- removeFB.sh
+++ removeFB.sh
@@ -1,16 +1,24 @@
 #!/bin/bash -eu
 
 BRANCH=feature/4.1.x-translation
+SCRIPTDIR=$(cd $(dirname "$0"); pwd)
+CURRENTDIR=$(pwd)
+
+SWF_FB_REPOS=${SWF_FB_REPOS:-$CURRENTDIR}
+#echo "FB source dirs = ${SWF_FB_REPOS}"
 
 function deleteFB(){
+  echo "########################################"
+  echo "##### repo : $(basename $1)"
+  echo "########################################"
   pushd $1
-  git checkout master
+  git checkout develop
   git branch -D $BRANCH || true
   git push origin --delete $BRANCH || true
   popd
 }
 
-
+pushd ${SWF_FB_REPOS}
 deleteFB platform-ui
 deleteFB commons
 deleteFB social
@@ -22,3 +30,5 @@ deleteFB integration
 deleteFB platform
 deleteFB platform-public-distributions
 deleteFB platform-private-distributions
+popd
+exit
