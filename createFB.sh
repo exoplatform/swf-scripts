135cd18e6cd5ecbe916e516d9a53f231cd495335
Frédéric DROUET <fdrouet@exoplatform.com>
2015-05-25 22:33:09 +0200

    Add some colors for better readability

--- createFB.sh
+++ createFB.sh
@@ -13,25 +13,31 @@ CURRENTDIR=$(pwd)
 SWF_FB_REPOS=${SWF_FB_REPOS:-$CURRENTDIR}
 
 function createFB(){
-  echo "########################################"
-  echo "# Repository: $1"
-  echo "########################################"
-  pushd $1
+  local repo_name=$1
+  printf "\e[1;33m########################################\e[m\n"
+  printf "\e[1;33m# Repository: %s\e[m\n" "${repo_name}"
+  printf "\e[1;33m########################################\e[m\n"
+  pushd ${repo_name}
 
   # Remove all branches but the origin one
 #  git checkout ${ORIGIN_BRANCH} && git branch | grep -v "${ORIGIN_BRANCH}" | xargs git branch -d -D
-
+  printf "\e[1;33m# %s\e[m\n" "Cleaning of ${repo_name} repository ..."
   git remote update --prune
   git reset --hard HEAD
   git checkout $ORIGIN_BRANCH
   git reset --hard HEAD
   git pull
+  printf "\e[1;33m# %s\e[m\n" "Testing if ${TARGET_BRANCH} branch doesn't already exists and reuse it ..."
   set +e
   git checkout $TARGET_BRANCH
   if [ "$?" -ne "0" ]; then
     git checkout -b $TARGET_BRANCH
+  else
+    printf "\e[1;35m# %s\e[m\n" "WARNING : the ${TARGET_BRANCH} branch already exists so we reuse it (you have 5 seconds to cancel with CTRL+C) ..."
+    sleep 5
   fi
   set -e
+  printf "\e[1;33m# %s\e[m\n" "Modifying versions in the POMs ..."
   replaceInPom.sh "<version>$ORIGIN_VERSION</version>" "<version>$TARGET_VERSION_PREFIX-$2-$BRANCH-SNAPSHOT</version>" ""
   replaceInPom.sh "<org.exoplatform.platform-ui.version>$ORIGIN_VERSION</org.exoplatform.platform-ui.version>" "<org.exoplatform.platform-ui.version>$TARGET_VERSION_PREFIX-plfui-$BRANCH-SNAPSHOT</org.exoplatform.platform-ui.version>"
   replaceInPom.sh "<org.exoplatform.commons.version>$ORIGIN_VERSION</org.exoplatform.commons.version>" "<org.exoplatform.commons.version>$TARGET_VERSION_PREFIX-commons-$BRANCH-SNAPSHOT</org.exoplatform.commons.version>"
@@ -45,6 +51,8 @@ function createFB(){
   replaceInPom.sh "<org.exoplatform.platform.distributions.version>$ORIGIN_VERSION</org.exoplatform.platform.distributions.version>" "<org.exoplatform.platform.distributions.version>$TARGET_VERSION_PREFIX-pkgpub-$BRANCH-SNAPSHOT</org.exoplatform.platform.distributions.version>"
 #  replaceInPom.sh "<org.exoplatform.ide.version>1.4.x-SNAPSHOT</org.exoplatform.ide.version>" "<org.exoplatform.ide.version>1.4.x-ide-$BRANCH-SNAPSHOT</org.exoplatform.ide.version>"
 #  replaceInPom.sh "<org.exoplatform.depmgt.version>9-SNAPSHOT</org.exoplatform.depmgt.version>" "<org.exoplatform.depmgt.version>9-$BRANCH-SNAPSHOT</org.exoplatform.depmgt.version>"
+
+  printf "\e[1;33m# %s\e[m\n" "Commiting and pushing the new $TARGET_BRANCH branch to origin ..."
   git commit -m"$ISSUE : Create $BRANCH branch and update projects versions" -a
   git push origin $TARGET_BRANCH --set-upstream
   git checkout develop
@@ -57,7 +65,7 @@ createFB platform-ui plfui
 createFB commons commons
 createFB social soc
 createFB ecms ecms
-createFB wiki wiki
+createFB wiki wiki 
 createFB forum forum
 createFB calendar cal
 createFB integration integ
