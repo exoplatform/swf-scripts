2943fabe98abd0aadc681e9c0cc7dd4fab391aab
Maxime Gréau <mgreau@exoplatform.com>
2015-07-08 09:07:11 +0200

    SWF-3331: No rebase, just push force to remote branch

--- jenkins/translation/synchronize-translation-branches.rb
+++ jenkins/translation/synchronize-translation-branches.rb
@@ -12,7 +12,8 @@ require 'net/https'
 # Synchonize develop branches with translation branches for PLF projects.
 #
 #  The goal is to get last commits on "develop" branches and to push those commits
-#  to the translation branches which may have been updated too.
+#  to the remote translation branches which may have been updated too, but translation commits can be lost
+#  because they will be re-injected by another process.
 #
 class SyncTranslationBranches
 
@@ -21,7 +22,7 @@ class SyncTranslationBranches
    # Branch name with last modifications
    SourceBranch = "develop"
    # Branch name to update with last modifications
-   TranslationBranch = "integration/4.3.x-translation"
+   TranslationBranch = "SWF-3331/4.3.x-translation"#"integration/4.3.x-translation"
 
    # Logs level
    INFO = "INFO"
@@ -51,12 +52,9 @@ class SyncTranslationBranches
     end
   end
 
-  # Process the synchronization between addons repositories
-  # 1. get all supported addons repositories
-  # 2. for each repos,
-  # 2.a clone the repo (if needed)
-  # 2.b set the fork origin url (parent)
-  # 2.c sync develop branch between 2 organizations
+  #
+  #
+  #
   def process
      print "[START] Synchronization process for Translations branches ($#{TranslationBranch}) started...\n\n"
 
@@ -138,21 +136,17 @@ class SyncTranslationBranches
     Dir.chdir repoName
   end
 
-  # chechout develop branch in exo-addons repository and push to develop branch in exoplatform repository
+  # chechout develop branch in eXo Dev repository and push to the translation remote branch
   def sync_project_branches(repoName)
     #develop branch
-    ok = self.resetBranch(repoName, SourceBranch)
+    ok = self.reset_branch(repoName, SourceBranch)
     if ok
-       #translation branch
-       ok = self.resetBranch(repoName, TranslationBranch)
-    end
-    if ok
-      #rebase and push
-      self.rebaseBranch(repoName, TranslationBranch, SourceBranch)
+      # push force to remote branch
+      self.push_force_to_remote_branch(repoName, SourceBranch, TranslationBranch)
     end
   end
 
-  def resetBranch(repoName, branchName)
+  def reset_branch(repoName, branchName)
     self.log(INFO,repoName,"Checkout #{branchName} branch (it is perhaps not the default) for #{repoName}...")
     s = system("git checkout #{branchName}")
     if !s
@@ -173,26 +167,17 @@ class SyncTranslationBranches
     end
   end
 
-  def rebaseBranch(repoName, branchToRebase, rebaseFromBranch)
-    s = system("git checkout #{branchToRebase}")
+  def push_force_to_remote_branch(repoName, sourceBranch, remoteBranch)
+    s = system("git checkout #{sourceBranch}")
     if !s
-      print("[ERROR] No #{branchToRebase} in repository #{repoName}, Skip this repo!!!\n")
+      print("[ERROR] No #{sourceBranch} in repository #{repoName}, Skip this repo!!!\n")
       self.log(INFO,repoName,"Done.")
       # Let's process the next one
     else
       self.log(INFO,repoName,"Done.")
-      self.log(INFO,repoName,"Rebase #{branchToRebase} on #{rebaseFromBranch} for #{repoName} ...")
-      s = system("git rebase -m #{rebaseFromBranch} #{branchToRebase}")
-      if !s
-        r = system("git rebase --skip")
-        if !r
-          while r == false
-            r = system("git rebase --skip")
-          end
-        end
-      end
-      self.log(INFO,repoName,"Rebase Done.")
-      #s = system("git push --force origin #{branchName}")
+      self.log(INFO,repoName,"Push force #{sourceBranch} to #{remoteBranch} for #{repoName} ...")
+      s = system("git push --force origin #{sourceBranch}:#{remoteBranch}")
+      self.log(INFO,repoName,"Push Done.")
     end
   end
 
