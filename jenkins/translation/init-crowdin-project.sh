d9ff791374433737af29bb416436b989600c3e52
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2017-06-26 15:19:35 +0200

    Move init translation of 1 project in a function

--- jenkins/translation/init-crowdin-project.sh
+++ jenkins/translation/init-crowdin-project.sh
@@ -15,22 +15,38 @@ LANGUAGES=en,ar,ca,nl,fr,de,el,it,ja,fa,pl,pt-BR,ru,zh-CN,sl,es-ES,tr,uk,vi,sq
 
 CROWDIN_MAVEN_PLUGIN_VERSION=1.2.x-SNAPSHOT
 
+##
+# Init translations in Crowdin for one project
+# Arguments:
+#   1: git repository (for example exoplatform/calendar)
+#   2: git branch (for example stable/4.4.x) 
+#
+function initTranslation() {
+  local gitRepo=$1
+  local gitBranch=$2
+  echo "############################################################################"
+  echo "# Project $gitRepo - branch $gitBranch"
+  echo "############################################################################"
+  echo "Clone git repo exoplatform/$project - branch $gitBranch"
+  pushd sources && /usr/bin/git clone git@github.com:$gitRepo.git
+  cd $project && /usr/bin/git branch ${gitBranch} origin/${gitBranch} && git checkout ${gitBranch}
+  echo "Upload translations of repo $gitRepo in Crowdin"
+  mvn org.exoplatform.translation.crowdin:crowdin-maven-plugin:${CROWDIN_MAVEN_PLUGIN_VERSION}:upload-translation -Dexo.crowdin.project.key=${CROWDIN_PROJECT_KEY} -Dexo.crowdin.project.id=${CROWDIN_PROJECT_NAME} -Dexo.crowdin.autoApprovedImported=true -Dlangs=${LANGUAGES}
+  popd
+}
+
 set -e
 mkdir -p sources
+
 projects=("gatein-portal:${PLF_VERSION}" "platform-ui:${PLF_VERSION}" "commons:${PLF_VERSION}" "ecms:${PLF_VERSION}" "social:${PLF_VERSION}" "wiki:${PLF_VERSION}" "forum:${PLF_VERSION}" "calendar:${PLF_VERSION}" "integration:${PLF_VERSION}" "platform:${PLF_VERSION}" "platform-public-distributions:${PLF_VERSION}" "platform-private-distributions:${PLF_VERSION}" "chat-application:${CHAT_ADDON_VERSION}" "task:${TASK_ADDON_VERSION}")
+
 for projectWithVersion in "${projects[@]}"
 do
   projectInfo=(${projectWithVersion//:/ })
   project=${projectInfo[0]}
   version=${projectInfo[1]}
+  gitRepo=exoplatform/$project
   gitBranch=stable/$version
-  echo "############################################################################"
-  echo "# Project exoplatform/$project - branch $gitBranch"
-  echo "############################################################################"
-  echo "Clone git repo exoplatform/$project - branch $gitBranch"
-  pushd sources && /usr/bin/git clone git@github.com:exoplatform/$project.git
-  cd $project && /usr/bin/git branch ${gitBranch} origin/${gitBranch} && git checkout ${gitBranch}
-  echo "Upload translations of repo $project in Crowdin"
-  mvn org.exoplatform.translation.crowdin:crowdin-maven-plugin:${CROWDIN_MAVEN_PLUGIN_VERSION}:upload-translation -Dexo.crowdin.project.key=${CROWDIN_PROJECT_KEY} -Dexo.crowdin.project.id=${CROWDIN_PROJECT_NAME} -Dexo.crowdin.autoApprovedImported=true -Dlangs=${LANGUAGES}
-  popd
+
+  initTranslation $gitRepo $gitBranch
 done
