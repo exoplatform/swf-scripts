9c52b6715ac91a81b91e19763c62869183d8df81
Vincent Sellier <vsellier@exoplatform.com>
2017-08-29 14:59:19 +0200

    SWF-4063: prepare organizationservice-cache FB

--- git-workflow/createFB.sh
+++ git-workflow/createFB.sh
@@ -2,8 +2,8 @@
 
 # Create Git Feature Branches for PLF projects
 
-BRANCH=multi-tag-search
-ISSUE=SWF-4061
+BRANCH=organizationservice-cache
+ISSUE=SWF-4063
 ORIGIN_BRANCH=develop
 TARGET_BRANCH=feature/$BRANCH
 ORIGIN_VERSION=5.0.x-SNAPSHOT
