929b43ac34818e34291139ec4a0a28cfa69da777
Frédéric DROUET <fdrouet@exoplatform.com>
2017-09-21 12:10:44 +0200

    Allow to specify an alternative version for sed command

--- replaceInFile.sh
+++ replaceInFile.sh
@@ -1,3 +1,2 @@
 SEP="`echo | tr '\n' '\001'`"
-find . -name $3 -exec sed -i "" "s${SEP}$1${SEP}$2${SEP}g" {} \;
-#find . -name pom.xml -exec sed -i ".sav" "s#$1#$2#g" {} \;
+find . -name $3 -exec ${EXO_FB_SED:-sed} -i "" "s${SEP}$1${SEP}$2${SEP}g" {} \;
