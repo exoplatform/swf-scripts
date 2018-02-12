075976ce744d5382ac0fe883932f69d7c3404e4c
Vincent Sellier <vsellier@exoplatform.com>
2018-02-12 16:57:50 +0100

    fix syntax

--- replaceInFile.sh
+++ replaceInFile.sh
@@ -1,9 +1,13 @@
 SEP="`echo | tr '\n' '\001'`"
-if [ "${OSTYPE}" == darwin* ]; then
-    find . -name $3 -exec ${EXO_FB_SED:-sed} -i "" "s${SEP}$1${SEP}$2${SEP}g" {} \;
-elif [ "${OSTYPE}" == linux* ]; then
-    find . -name $3 -exec ${EXO_FB_SED:-sed} -i "s${SEP}$1${SEP}$2${SEP}g" {} \;
-else
-    echo "Unsupported OS '${OSTYPE}'"
-    exit 1
-fi
+
+case $OSTYPE in
+    darwin*)
+        find . -name $3 -exec ${EXO_FB_SED:-sed} -i "" "s${SEP}$1${SEP}$2${SEP}g" {} \;
+    ;;
+    linux*)
+        find . -name $3 -exec ${EXO_FB_SED:-sed} -i "s${SEP}$1${SEP}$2${SEP}g" {} \;
+    ;;
+    default)
+        echo "Unsupported OS '${OSTYPE}'"
+        exit 1
+esac
