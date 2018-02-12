1e5650f21d253497f0c2716e4b5529b14a8aca2f
Vincent Sellier <vsellier@exoplatform.com>
2018-02-12 16:47:38 +0100

    Support replacing file content on linux

--- replaceInFile.sh
+++ replaceInFile.sh
@@ -1,2 +1,9 @@
 SEP="`echo | tr '\n' '\001'`"
-find . -name $3 -exec ${EXO_FB_SED:-sed} -i "" "s${SEP}$1${SEP}$2${SEP}g" {} \;
+if [ "${OSTYPE}" == darwin* ]; then
+    find . -name $3 -exec ${EXO_FB_SED:-sed} -i "" "s${SEP}$1${SEP}$2${SEP}g" {} \;
+elif [ "${OSTYPE}" == linux* ]; then
+    find . -name $3 -exec ${EXO_FB_SED:-sed} -i "s${SEP}$1${SEP}$2${SEP}g" {} \;
+else
+    echo "Unsupported OS '${OSTYPE}'"
+    exit 1
+fi
