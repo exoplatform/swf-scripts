5f7e2f3340b5a59a724b939f18c65f137be7f540
Vincent Sellier <vsellier@exoplatform.com>
2018-02-12 17:08:19 +0100

    change separator

--- replaceInFile.sh
+++ replaceInFile.sh
@@ -1,10 +1,11 @@
-SEP="`echo | tr '\n' '\001'`"
 
 case $OSTYPE in
     darwin*)
+        SEP="`echo | tr '\n' '\001'`"   ## WHY ?
         find . -name $3 -exec ${EXO_FB_SED:-sed} -i "" "s${SEP}$1${SEP}$2${SEP}g" {} \;
     ;;
     linux*)
+        SEP="|"   ## WHY ?
         find . -name $3 -exec ${EXO_FB_SED:-sed} -i "s${SEP}$1${SEP}$2${SEP}g" {} \;
     ;;
     default)
