d451f72592dcf3b52b686d5a7869e06561badc43
Vincent Sellier <vsellier@exoplatform.com>
2018-09-27 01:00:38 +0200

    SWF-4454 update cf-parent and maven-parent-pom dependencies

--- maven/cf-parent.sh
+++ maven/cf-parent.sh
@@ -4,9 +4,9 @@ REMOTE=origin
 #REPLACE_WHAT="<org.gatein.portal.version>3.5.10.Final-SNAPSHOT</org.gatein.portal.version>"
 #REPLACE_BY="<org.gatein.portal.version>3.5.11.Final-SNAPSHOT</org.gatein.portal.version>"
 #COMMIT_MSG="Update Gatein 3.5.10.Final-SNAPSHOT -> 3.5.10.Final-SNAPSHOT"
-REPLACE_WHAT="<version>15-RC02</version>"
-REPLACE_BY="<version>15</version>"
-COMMIT_MSG="RELEASE-175: Upgrade cf-parent 15-RC02 -> 15"
+REPLACE_WHAT="<version>17-RC01</version>"
+REPLACE_BY="<version>17</version>"
+COMMIT_MSG="SWF-4454: Release maven parent pom for 5.1.0-GA"
 #REPLACE_WHAT="<version>13-SNAPSHOT</version>"
 #REPLACE_BY="<version>13</version>"
 #COMMIT_MSG="Use maven-parent-pom 13"
@@ -21,7 +21,7 @@ updateProject (){
   echo "================================================================================"
   pushd $1
   git remote update 
-  git co develop
+  git checkout develop
    
   local LOCAL_BRANCH=$2
   local REMOTE_BRANCH=$REMOTE/$LOCAL_BRANCH
@@ -29,7 +29,7 @@ updateProject (){
   git branch -D $LOCAL_BRANCH
   git checkout -b $LOCAL_BRANCH $REMOTE/$LOCAL_BRANCH
   git reset --hard $REMOTE_BRANCH
-  git pullr
+  git pull
 	#git branch --set-upstream-to=$REMOTE/$LOCAL_BRANCH $LOCAL_BRANCH
 
   $SCRIPTDIR/../replaceInFile.sh "$REPLACE_WHAT" "$REPLACE_BY" "pom.xml -not -wholename \"*/target/*\""
@@ -41,8 +41,7 @@ updateProject (){
   popd
 }
 
-updateProject kernel stable/2.6.x
-updateProject core stable/2.7.x
-updateProject ws stable/2.5.x
-updateProject jcr stable/1.17.x
-updateProject jcr-services stable/1.17.x
+updateProject kernel stable/5.1.x
+updateProject core stable/5.1.x
+updateProject ws stable/5.1.x
+updateProject jcr stable/5.1.x
--- maven/update-parent-pom-version.sh
+++ maven/update-parent-pom-version.sh
@@ -18,10 +18,10 @@ done
 REMOTE=origin
 LOCAL_BRANCH=develop
 
-PREVIOUS_VERSION=9-M02
-NEXT_VERSION=9-RC01
-ISSUE=SWF-4382
-PROJECT=addons-parent-pom
+PREVIOUS_VERSION=18-RC01
+NEXT_VERSION=18
+ISSUE=SWF-4454
+PROJECT=maven-parent-pom
 
 #REPLACE_WHAT="<org.gatein.portal.version>3.5.10.Final-SNAPSHOT</org.gatein.portal.version>"
 #REPLACE_BY="<org.gatein.portal.version>3.5.11.Final-SNAPSHOT</org.gatein.portal.version>"
@@ -61,7 +61,7 @@ updateProject() {
 	$SCRIPTDIR/../replaceInPom.sh "$REPLACE_WHAT" "$REPLACE_BY"
 	# Test if there is any changes to commit
 	pwd
-	CHANGED=$(git diff --name-only)
+	CHANGED=$(git diff -w)
 	if [ -n "${CHANGED}" ]; then
 		git --no-pager diff
 		pause "Press [Enter] key to continue... We will commit on project ${project} with message : $COMMIT_MSG"
@@ -79,50 +79,52 @@ if $PUSH_CHANGES; then
 fi
 
 # Supported
-updateProject juzu master
+updateProject addons-parent-pom stable/9.x
 
-updateProject gatein-wci
-updateProject kernel
-updateProject core
-updateProject ws
-updateProject jcr
-updateProject gatein-dep
-updateProject gatein-sso
-updateProject gatein-pc
-updateProject gatein-portal
+updateProject juzu stable/1.2.x
+
+updateProject gatein-wci stable/5.1.x
+updateProject kernel stable/5.1.x
+updateProject core stable/5.1.x
+updateProject ws stable/5.1.x
+updateProject jcr stable/5.1.x
+updateProject gatein-dep stable/1.6.x
+updateProject gatein-sso stable/5.1.x
+updateProject gatein-pc stable/5.1.x
+updateProject gatein-portal stable/5.1.x
 
 ## PLF
-updateProject docs-style
-updateProject platform-ui
-updateProject commons
-updateProject ecms
-updateProject social
-updateProject wiki
-updateProject forum
-updateProject calendar
-updateProject integration
-updateProject platform
+updateProject docs-style stable/5.1.x
+updateProject platform-ui stable/5.1.x
+updateProject commons stable/5.1.x
+updateProject ecms stable/5.1.x
+updateProject social stable/5.1.x
+updateProject wiki stable/5.1.x
+updateProject forum stable/5.1.x
+updateProject calendar stable/5.1.x
+updateProject integration stable/5.1.x
+updateProject platform stable/5.1.x
 
 ## Addons
-updateProject addons-manager
-updateProject answers
-updateProject cas-addon
-updateProject chat-application
-updateProject cmis-addon
+updateProject addons-manager stable/1.3.x
+updateProject answers stable/2.1.x
+updateProject cas-addon stable/2.1.x
+updateProject chat-application stable/2.1.x
+updateProject cmis-addon stable/5.1.x
 # updateProject crash-addon
-updateProject exo-es-embedded
+updateProject exo-es-embedded stable/2.1.x
 
-updateProject enterprise-skin
-updateProject openam-addon
-updateProject remote-edit
-updateProject saml2-addon
-updateProject spnego-addon
-updateProject task
-updateProject wcm-template-pack
-updateProject web-conferencing
-updateProject push-notifications
+updateProject enterprise-skin stable/5.1.x
+updateProject openam-addon stable/2.1.x
+updateProject remote-edit stable/2.1.x
+updateProject saml2-addon stable/2.1.x
+updateProject spnego-addon stable/2.1.x
+updateProject task stable/2.1.x
+updateProject wcm-template-pack stable/2.1.x
+updateProject web-conferencing stable/1.2.x
+updateProject push-notifications stable/1.0.x
 
 ## Distrib
-updateProject platform-public-distributions
-updateProject platform-private-distributions
-updateProject platform-private-trial-distributions
+updateProject platform-public-distributions stable/5.1.x
+updateProject platform-private-distributions stable/5.1.x
+updateProject platform-private-trial-distributions stable/5.1.x
