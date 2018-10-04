f5d93cb66b53be68156b230fff80f528baa4d8a0
Vincent Sellier <vsellier@exoplatform.com>
2018-10-04 12:44:14 +0200

    SWF-4468 Upgrade addons-parent-pom dependencies for 5.2.0-M01

--- maven/update-parent-pom-version.sh
+++ maven/update-parent-pom-version.sh
@@ -18,10 +18,10 @@ done
 REMOTE=origin
 LOCAL_BRANCH=develop
 
-PREVIOUS_VERSION=19-SNAPSHOT
-NEXT_VERSION=19-M01
+PREVIOUS_VERSION=10-SNAPSHOT
+NEXT_VERSION=10-M01
 ISSUE=SWF-4468
-PROJECT=maven-parent-pom
+PROJECT=addons-parent-pom
 
 #REPLACE_WHAT="<org.gatein.portal.version>3.5.10.Final-SNAPSHOT</org.gatein.portal.version>"
 #REPLACE_BY="<org.gatein.portal.version>3.5.11.Final-SNAPSHOT</org.gatein.portal.version>"
@@ -84,7 +84,7 @@ if $PUSH_CHANGES; then
 fi
 
 # Supported
-updateProject addons-parent-pom # stable/9.x
+# updateProject addons-parent-pom # stable/9.x
 
 updateProject juzu master # # stable/1.2.x
 
