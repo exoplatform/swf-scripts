eb784e00b2fcf1c062b4395abb13505685401aae
Vincent Sellier <vsellier@exoplatform.com>
2019-01-23 18:43:48 +0100

    SWF-4652 Migrate cf-parent from 18-M01 to 18-RC01

--- maven/update-parent-pom-version.sh
+++ maven/update-parent-pom-version.sh
@@ -18,10 +18,10 @@ done
 REMOTE=origin
 LOCAL_BRANCH=develop
 
-PREVIOUS_VERSION=19-M01
-NEXT_VERSION=19-RC01
+PREVIOUS_VERSION=18-M01
+NEXT_VERSION=18-RC01
 ISSUE=SWF-4652
-PROJECT=maven-parent-pom
+PROJECT=cf-parent
 
 #REPLACE_WHAT="<org.gatein.portal.version>3.5.10.Final-SNAPSHOT</org.gatein.portal.version>"
 #REPLACE_BY="<org.gatein.portal.version>3.5.11.Final-SNAPSHOT</org.gatein.portal.version>"
