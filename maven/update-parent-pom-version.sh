f4dc7f0ea12841f5f7e580b5ad4ed7f24aeb979d
Vincent Sellier <vsellier@exoplatform.com>
2019-10-03 17:22:10 +0200

    Update maven-parent-pom dependencies before 6.0.0-M01

--- maven/update-parent-pom-version.sh
+++ maven/update-parent-pom-version.sh
@@ -18,10 +18,10 @@ done
 REMOTE=origin
 LOCAL_BRANCH=develop
 
-PREVIOUS_VERSION=10
-NEXT_VERSION=11-RC01
-ISSUE=SWF-4943
-PROJECT=addons-parent-pom
+PREVIOUS_VERSION=20-RC01
+NEXT_VERSION=21-M01
+ISSUE=SWF-4953
+PROJECT=maven-parent-pom
 
 #REPLACE_WHAT="<org.gatein.portal.version>3.5.10.Final-SNAPSHOT</org.gatein.portal.version>"
 #REPLACE_BY="<org.gatein.portal.version>3.5.11.Final-SNAPSHOT</org.gatein.portal.version>"
