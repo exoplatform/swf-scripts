bca646e27ad7171fbfa79df5221cfbd35ef6ab0d
Vincent Sellier <vsellier@exoplatform.com>
2019-12-05 20:30:48 +0100

    SWF-5028 upgrade maven-parent-pom for PLF 5.3.0

--- maven/update-parent-pom-version.sh
+++ maven/update-parent-pom-version.sh
@@ -12,28 +12,24 @@ while getopts "pd" option; do
 		echo "Activate debug"
 		set -x
 		;;
+	*)
+		echo "unsupported option ${option}"
+		exit 1
+		;;
 	esac
 done
 
 REMOTE=origin
 LOCAL_BRANCH=develop
 
-PREVIOUS_VERSION=11-RC01
-NEXT_VERSION=12-M01
-ISSUE=SWF-4953
-PROJECT=addons-parent-pom
-
-#REPLACE_WHAT="<org.gatein.portal.version>3.5.10.Final-SNAPSHOT</org.gatein.portal.version>"
-#REPLACE_BY="<org.gatein.portal.version>3.5.11.Final-SNAPSHOT</org.gatein.portal.version>"
-#COMMIT_MSG="Update Gatein 3.5.10.Final-SNAPSHOT -> 3.5.10.Final-SNAPSHOT"
+PREVIOUS_VERSION=20-RC01
+NEXT_VERSION=20
+ISSUE=SWF-5028
+PROJECT=maven-parent-pom
 
 REPLACE_WHAT="<version>${PREVIOUS_VERSION}</version>"
 REPLACE_BY="<version>${NEXT_VERSION}</version>"
 COMMIT_MSG="${ISSUE}: Upgrade ${PROJECT} ${PREVIOUS_VERSION} -> ${NEXT_VERSION}"
-
-#REPLACE_WHAT="<version>13-SNAPSHOT</version>"
-#REPLACE_BY="<version>13</version>"
-#COMMIT_MSG="Use maven-parent-pom 13"
 
 SCRIPTDIR=$(
 	cd $(dirname "$0")
@@ -84,62 +80,62 @@ if $PUSH_CHANGES; then
 fi
 
 # Supported
-# updateProject addons-parent-pom # stable/9.x
+updateProject addons-parent-pom stable/11.x
+updateProject maven-depmgt-pom stable/16.x
+# updateProject juzu master # # stable/1.2.x
 
-updateProject juzu master # # stable/1.2.x
-
-updateProject gatein-wci # stable/5.1.x
-updateProject kernel # stable/5.1.x
-updateProject core # stable/5.1.x
-updateProject ws # stable/5.1.x
-updateProject jcr # stable/5.1.x
-updateProject gatein-dep # stable/1.6.x
-updateProject gatein-sso # stable/5.1.x
-updateProject gatein-pc # stable/5.1.x
-updateProject gatein-portal # stable/5.1.x
+updateProject gatein-wci stable/5.3.x
+updateProject kernel stable/5.3.x
+updateProject core stable/5.3.x
+updateProject ws stable/5.3.x
+updateProject jcr stable/5.3.x
+updateProject gatein-dep stable/1.8.x
+updateProject gatein-sso stable/5.3.x
+updateProject gatein-pc stable/5.3.x
+updateProject gatein-portal stable/5.3.x
 
 ## PLF
-updateProject docs-style # stable/5.1.x
-updateProject platform-ui # stable/5.1.x
-updateProject commons # stable/5.1.x
-updateProject ecms # stable/5.1.x
-updateProject social # stable/5.1.x
-updateProject wiki # stable/5.1.x
-updateProject forum # stable/5.1.x
-updateProject calendar # stable/5.1.x
-updateProject integration # stable/5.1.x
-updateProject platform # stable/5.1.x
+updateProject docs-style stable/5.3.x
+updateProject platform-ui stable/5.3.x
+updateProject commons stable/5.3.x
+updateProject ecms stable/5.3.x
+updateProject social stable/5.3.x
+updateProject wiki stable/5.3.x
+updateProject forum stable/5.3.x
+updateProject calendar stable/5.3.x
+updateProject integration stable/5.3.x
+updateProject platform stable/5.3.x
 
 ## Addons
-updateProject addons-manager # stable/1.3.x
+updateProject addons-manager # stable/1.5.x
 # Not supported starting from 5.2.0
 # updateProject answers # stable/2.1.x
-updateProject cas-addon # stable/2.1.x
-updateProject chat-application # stable/2.1.x
-updateProject cmis-addon # stable/5.1.x
-updateProject enterprise-skin # stable/5.1.x
-updateProject openam-addon # stable/2.1.x
-updateProject remote-edit # stable/2.1.x
-updateProject saml2-addon # stable/2.1.x
-updateProject spnego-addon # stable/2.1.x
-updateProject task # stable/2.1.x
-updateProject wcm-template-pack # stable/2.1.x
-updateProject web-conferencing # stable/1.2.x
-updateProject push-notifications # stable/1.0.x
+updateProject cas-addon stable/2.3.x
+updateProject chat-application stable/2.3.x
+updateProject cmis-addon stable/5.3.x
+updateProject enterprise-skin stable/5.3.x
+updateProject openam-addon stable/2.3.x
+updateProject remote-edit stable/2.3.x
+updateProject saml2-addon stable/2.3.x
+updateProject spnego-addon stable/2.3.x
+updateProject task stable/2.3.x
+updateProject wcm-template-pack stable/2.3.x
+updateProject web-conferencing stable/1.4.x
+updateProject push-notifications stable/1.2.x
 # Not supported starting 5.1.x
 # updateProject crash-addon
-updateProject exo-es-embedded # stable/2.1.x
+updateProject exo-es-embedded stable/2.3.x
 # Supported since 5.2.0
-updateProject lecko # stable/5.1.x
+updateProject lecko stable/1.4.x
 # Supported since 5.3.0
-updateProject wallet # stable/5.1.x
-updateProject perk-store # stable/5.1.x
-updateProject kudos # stable/5.1.x
-updateProject gamification # stable/5.1.x
+updateProject wallet stable/1.0.x
+updateProject perk-store stable/1.1.x
+updateProject kudos stable/1.1.x
+updateProject gamification stable/1.2.x
 
 
 ## Distrib
-updateProject platform-public-distributions # stable/5.1.x
-updateProject platform-private-distributions # stable/5.1.x
+updateProject platform-public-distributions stable/5.3.x
+updateProject platform-private-distributions stable/5.3.x
 # Not released since 5.3.0
 #updateProject platform-private-trial-distributions # stable/5.1.x
