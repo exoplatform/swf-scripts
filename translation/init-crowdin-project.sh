d94aa556de56aac06b856ed9f12dbd1af103ef74
bourasom <obouras@exoplatform.com>
2020-04-09 17:57:43 +0200

    ITOP-4585: Populate the 5.3 project with the current translations

--- translation/init-crowdin-project.sh
+++ translation/init-crowdin-project.sh
@@ -9,17 +9,18 @@
 
 
 # Stable versions
-PLF_VERSION=5.2.x
-CHAT_ADDON_VERSION=2.2.x
-NEWS_VERSION=1.0.x
-TASK_ADDON_VERSION=2.2.x
-WEB_CONFERENCING_ADDON_VERSION=1.3.x
-WEB_PACK_ADDON_VERSION=2.2.x
-PUSH_NOTIFICATIONS_ADDON_VERSION=1.1.x
+PLF_VERSION=5.3.x
+CHAT_ADDON_VERSION=2.3.x
+NEWS_VERSION=1.1.x
+TASK_ADDON_VERSION=2.3.x
+WEB_CONFERENCING_ADDON_VERSION=1.4.x
+WEB_PACK_ADDON_VERSION=2.3.x
+PUSH_NOTIFICATIONS_ADDON_VERSION=1.2.x
 WALLET_ADDON_VERSION=1.0.x
 KUDOS_ADDON_VERSION=1.1.x
 PERKSTORE_ADDON_VERSION=1.1.x
-GAMIFICATION_VERSION=1.0.x
+GAMIFICATION_VERSION=1.2.x
+ONLYOFFICE_VERSION=1.5.x
 
 CROWDIN_PROJECT_NAME=$1
 CROWDIN_PROJECT_KEY=$2
@@ -52,7 +53,7 @@ function initTranslation() {
 set -e
 mkdir -p sources
 
-projects=("gatein-portal:${PLF_VERSION}" "platform-ui:${PLF_VERSION}" "commons:${PLF_VERSION}" "ecms:${PLF_VERSION}" "social:${PLF_VERSION}" "wiki:${PLF_VERSION}" "forum:${PLF_VERSION}" "calendar:${PLF_VERSION}" "integration:${PLF_VERSION}" "platform:${PLF_VERSION}" "platform-public-distributions:${PLF_VERSION}" "platform-private-distributions:${PLF_VERSION}" "chat-application:${CHAT_ADDON_VERSION}" "push-notifications:${PUSH_NOTIFICATIONS_ADDON_VERSION}" "task:${TASK_ADDON_VERSION}" "wcm-template-pack:${WEB_PACK_ADDON_VERSION}" "web-conferencing:${WEB_CONFERENCING_ADDON_VERSION}" "wallet:${WALLET_ADDON_VERSION}" "kudos:${KUDOS_ADDON_VERSION}" "perk-store:${PERKSTORE_ADDON_VERSION}" "gamification:${GAMIFICATION_VERSION}" "news:${NEWS_VERSION}") 
+projects=("gatein-portal:${PLF_VERSION}" "platform-ui:${PLF_VERSION}" "commons:${PLF_VERSION}" "ecms:${PLF_VERSION}" "social:${PLF_VERSION}" "wiki:${PLF_VERSION}" "forum:${PLF_VERSION}" "calendar:${PLF_VERSION}" "integration:${PLF_VERSION}" "platform:${PLF_VERSION}" "platform-public-distributions:${PLF_VERSION}" "platform-private-distributions:${PLF_VERSION}" "chat-application:${CHAT_ADDON_VERSION}" "push-notifications:${PUSH_NOTIFICATIONS_ADDON_VERSION}" "task:${TASK_ADDON_VERSION}" "wcm-template-pack:${WEB_PACK_ADDON_VERSION}" "web-conferencing:${WEB_CONFERENCING_ADDON_VERSION}" "wallet:${WALLET_ADDON_VERSION}" "kudos:${KUDOS_ADDON_VERSION}" "perk-store:${PERKSTORE_ADDON_VERSION}" "gamification:${GAMIFICATION_VERSION}" "news:${NEWS_VERSION}" "onlyoffice:${ONLYOFFICE_VERSION}") 
 
 
 for projectWithVersion in "${projects[@]}"
