184e50236c54aa23e40af766a0d8c25452587ccc
hbenali <hbenali@exoplatform.com>
2020-04-30 17:39:50 +0200

    TA-24099: Remove Jira Integration from Catalog updater

--- catalog/Readme.md
+++ catalog/Readme.md
@@ -2,14 +2,6 @@
 
 ## Configuration
 
-* Install the [go_jira](https://github.com/go-jira/jira/releases) tool
-* Configure ``go_jira`` by creating a ``$HOME/.jira.d/config.yml`` file :
-
-```
-endpoint: https://jira.exoplatform.org
-user: ...
-```
-
 * Create a ``$HOME/.catalog.env`` file with this content :
 
 ```
@@ -21,22 +13,15 @@ CATALOG_PATH=...
 ## Command
 
 ```
-./catalog.sh -j JIRA_ID [-r] [-e env] [-c customer]
+./catalog.sh -o operation [-e env] [-c customer]
 ```
-* JIRA_ID : [mandatory] the jira issue asking for the catalog update
-* -r : [optional] Change the status of the issue to resolve after the catalog is updated (The initial status of the jira must be IN_PROGRESS)
+* -o : [mandatory] the operation to be performed: VIEW or VALIDATE are accepted
 * -e : [optional] Generate the catalog for a specific environment (hosting|acceptance)
 * -c : [optional] Generate the catalog for a specific customer
 
 Example :
 ```bash
-$ ./catalog.sh -j XXXX-4653
-Jira issue : XXXX-4653
-issue: XXXX-4653
-created: 20 hours ago
-status: In Progress
-summary: Release PLF 6.0.0-M18
-Is this correct (Y/n) ?
+$ ./catalog.sh -o VALIDATE
 Downloading new catalog....
   % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                  Dload  Upload   Total   Spent    Left  Speed
@@ -45,8 +30,6 @@ Downloading new catalog....
 Download old catalog...
 list.json                                                                                          100% 2633KB   4.0MB/s   00:00
 Comparing catalogs....
-Preparing commit message...
-Update catalog (Y/n) ? y
 Updating catalog....
    Uploading new catalog...
 list-new.json                                                                                      100% 2658KB   5.8MB/s   00:00
@@ -62,8 +45,4 @@ Copying list.json to 20200207_111708-list.json
 removed `/tmp/list-new.json'
 Connection to xxx.exoplatform.org closed.
 Catalog updated
-Comment jira issue XXXX-4653
-OK ITOP-4653 https://jira.exoplatform.org/browse/XXXX-4653
-Changing status of XXXX-4653 to resolved ...
-OK ITOP-4654 https://jira.exoplatform.org/browse/XXXX-4653
 ```
--- catalog/catalog.sh
+++ catalog/catalog.sh
@@ -16,45 +16,12 @@ if [ -z "${CATALOG_URL}" ]; then
 fi
 set -u
 
-create_view_template() {
-	mkdir -p ~/.jira.d/templates
-	cat <<EOF >~/.jira.d/templates/catalog_view
-{{/* view template */ -}}
-issue: {{ .key }} 
-{{if .fields.created -}} 
-created: {{ .fields.created | age }} ago
-{{end -}}
-{{if .fields.status -}}
-status: {{ .fields.status.name }}
-{{end -}}
-summary: {{ .fields.summary }}
-EOF
-
-}
-
-display_jira_issue() {
-	jira view -t catalog_view $1
-}
-
-comment_jira_issue() {
-	jira comment --noedit $1 -m "$(cat $2)"
-}
-
-resolve_jira_issue() {
-	jira resolve $1
-}
-
-RESOLVE_ISSUE=false
 CUSTOMER=''
 ENVIRONMENT=''
-JIRA_ID=''
+OPERATION=''
 
-while getopts "rc:e:j:" opt; do
+while getopts "c:e:o:" opt; do
 	case $opt in
-	r)
-		echo "Jira issue will be resolved"
-		RESOLVE_ISSUE=true
-		;;
 	c)
 		CUSTOMER=${OPTARG}
 		echo "Catalog will be generated for customer ${CUSTOMER}"
@@ -63,35 +30,16 @@ while getopts "rc:e:j:" opt; do
 		ENVIRONMENT=${OPTARG}
 		echo "Catalog will be generated for environment ${ENVIRONMENT}"
 		;;
-	j)
-		JIRA_ID=${OPTARG}
-		echo "Jira issue : ${JIRA_ID}"
+	o)
+		OPERATION=${OPTARG}
 		;;
 	*)
-	        echo "Unsupported option -${opt}"
+		echo "Unsupported option -${opt}"
 		exit 1
 		;;
 	esac
 done
 
-if [ -z "${JIRA_ID}" ]; then
-	echo "You must provide a -j <issue> parameter"
-	exit 1
-fi
-
-create_view_template
-
-display_jira_issue ${JIRA_ID}
-
-read -p "Is this correct (Y/n) ? " response
-response=${response:-Y}
-
-if [ "$response" != "y" -a "$response" != "Y" ]; then
-	echo "Aborting catalog update"
-	exit 1
-fi
-
-echo "Downloading new catalog...."
 REQ_PARAMS='catalog=official&show=snapshot'
 CATALOG_FILE_NAME="list.json"
 if [ -n "${CUSTOMER}" ]; then
@@ -106,42 +54,31 @@ elif [ -n "${ENVIRONMENT}" ]; then
 	CATALOG_FILE_NAME="${ENVIRONMENT}-list.json"
 fi
 
+if [[ ! ${OPERATION} =~ ^(VIEW|VALIDATE)$ ]]; then
+	echo "You must provide an operation (VIEW|VALIDATE)"
+	exit 1
+fi
+echo "Operation ${OPERATION} will be performed by user: $USER"
+
+echo "Downloading new catalog...."
 curl -f -L "${CATALOG_URL}/exec?${REQ_PARAMS}" >/tmp/list-new.json
 echo "Download old catalog..."
 if ! scp ${CATALOG_HOST}:${CATALOG_PATH}/${CATALOG_FILE_NAME} /tmp/list-old.json; then
-  echo Unable to find old remote catalog
-  
-  read -p "Continue (Y/n) ? " response
-  response=${response:-Y}
-
-  if [ "$response" != "y" -a "$response" != "Y" ]; then
-    echo "Aborting..." 
-    exit 1
-  else
-    rm -f /tmp/list-old.json
-    touch /tmp/list-old.json
-  fi
+	echo Unable to find old remote catalog
+	rm -f /tmp/list-old.json
+	touch /tmp/list-old.json
 fi
 
 echo "Comparing catalogs...."
 set +e
 diff -U3 /tmp/list-old.json /tmp/list-new.json >/tmp/list.diff
 set -e
-echo "Preparing commit message..."
-if [ -n "${CUSTOMER}" ]; then
-	echo "${CUSTOMER} - ${ENVIRONMENT} - catalog updated :" >/tmp/comment
-else
-	echo "catalog updated :" >/tmp/comment
+if [ ! -s /tmp/list.diff ]; then
+	echo "Catalog is up to date"
+	exit
 fi
-echo "{noformat}" >>/tmp/comment
-cat /tmp/list.diff >>/tmp/comment
-echo "{noformat}" >>/tmp/comment
-less /tmp/comment
-
-read -p "Update catalog (Y/n) ? " response
-response=${response:-Y}
-
-if [ "$response" == "y" -o "$response" == "Y" ]; then
+cat /tmp/list.diff
+if [ ${OPERATION} == "VALIDATE" ]; then
 	echo "Updating catalog...."
 	echo "   Uploading new catalog..."
 	scp /tmp/list-new.json ${CATALOG_HOST}:/tmp/list-new.json
@@ -168,14 +105,5 @@ EOF
 	ssh ${CATALOG_HOST} chmod u+x update_catalog.sh
 	echo "   Executing script..."
 	ssh -t ${CATALOG_HOST} sudo ./update_catalog.sh
-
 	echo "Catalog updated"
-	echo "Comment jira issue ${JIRA_ID}"
-	comment_jira_issue $JIRA_ID /tmp/comment
-
-	if [ "${RESOLVE_ISSUE}" = "true" ]; then
-		echo "Changing status of ${JIRA_ID} to resolved ..."
-		resolve_jira_issue $JIRA_ID
-	fi
-
 fi
