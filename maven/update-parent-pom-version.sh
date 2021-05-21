0941e959fa9ffdad2be7a3ab030557f667a1a571
bourasom <obouras@exoplatform.com>
2021-05-21 15:02:11 +0200

    TASK-44549: update concerned projects by parent-pom version changes

--- maven/update-parent-pom-version.sh
+++ maven/update-parent-pom-version.sh
@@ -79,70 +79,51 @@ if $PUSH_CHANGES; then
 	pause "Enter if ok, ctrl+c to cancel"
 fi
 
-# Supported
-updateProject addons-parent-pom develop
-updateProject maven-depmgt-pom develop
-# updateProject juzu master # # stable/1.2.x
-
+#Meeds Projects
+updateProject gatein-dep develop
 updateProject gatein-wci develop
 updateProject kernel develop
 updateProject core develop
 updateProject ws develop
-updateProject jcr develop
-updateProject gatein-dep develop
-updateProject gatein-sso develop
 updateProject gatein-pc develop
+updateProject gatein-sso develop
 updateProject gatein-portal develop
-
-## PLF
-#updateProject docs-style develop
+updateProject maven-depmgt-pom develop
 updateProject platform-ui develop
 updateProject commons develop
-updateProject ecms develop
 updateProject social develop
-updateProject wiki develop
-updateProject forum develop
-updateProject calendar develop
-#updateProject integration develop
-updateProject platform develop
+updateProject addons-manager develop
+updateProject app-center develop
+updateProject gamification develop
+updateProject kudos develop
+updateProject perk-store develop
+updateProject exo-es-embedded develop
+updateProject wallet develop
+updateProject meeds develop
+updateProject push-notifications develop
+updateProject notes develop
 
-## Addons
-updateProject addons-manager # stable/1.5.x
-# Not supported starting from 5.2.0
-# updateProject answers # stable/2.1.x
+# Explatform Projects
+updateProject ecms develop
+updateProject jcr develop
+updateProject agenda develop
+updateProject analytics develop
+updateProject jitsi develop
+updateProject jitsi-call develop
 updateProject cas-addon develop
 updateProject chat-application develop
 updateProject cmis-addon develop
-#updateProject enterprise-skin develop
-updateProject openam-addon develop
+updateProject data-upgrade develop
+updateProject digital-workplace develop
+updateProject layout-management develop
+updateProject legacy-intranet develop
+updateProject news develop
+updateProject onlyoffice develop
 updateProject remote-edit develop
 updateProject saml2-addon develop
 updateProject spnego-addon develop
 updateProject task develop
-updateProject wcm-template-pack develop
 updateProject web-conferencing develop
-updateProject push-notifications develop
-# Not supported starting 5.1.x
-# updateProject crash-addon
-updateProject exo-es-embedded develop
-# Supported since 5.2.0
-updateProject lecko develop
-# Supported since 5.3.0
-updateProject wallet develop
-updateProject perk-store develop
-updateProject kudos develop
-updateProject gamification develop
-# Supported since 6.0.x
-updateProject news develop
-updateProject legacy-intranet develop
-updateProject app-center develop
-updateProject layout-management develop
-updateProject digital-workplace develop
 updateProject data-upgrade develop
-updateProject onlyoffice develop
-
-
-
-## Distrib
-updateProject meeds develop
+updateProject analytics develop
 updateProject platform-private-distributions develop
