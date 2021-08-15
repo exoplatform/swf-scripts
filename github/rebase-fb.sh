ed0a7bcce2cde0a59604f0d80d2dece93618230a
Houssem Ben Ali <hbenali@exoplatform.com>
2021-08-15 16:58:05 +0100

    Colorize dangled commits

--- github/rebase-fb.sh
+++ github/rebase-fb.sh
@@ -49,7 +49,7 @@ while IFS= read -r line; do
     if [ ! -z "$(git diff origin/feature/${FB_NAME} 2>/dev/null)" ]; then
       info "Changes before the rebase:"
       echo -e "\033[1;32m****\033[0m"
-      git log HEAD..origin/feature/${FB_NAME} --oneline --format="(%h) %s"
+      git log HEAD..origin/feature/${FB_NAME} --oneline --pretty=format="(%C(yellow)%h%Creset) %s"
       echo -e "\033[1;32m****\033[0m"
     else 
       info "No changes detected!"  
