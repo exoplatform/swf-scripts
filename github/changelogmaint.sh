e4a4d62449272c0985bacf7e554dd289bae30b28
Houssem Ben Ali <hbenali@exoplatform.com>
2022-02-09 05:10:35 +0000

    fixmaint

--- github/changelogmaint.sh
+++ github/changelogmaint.sh
@@ -43,6 +43,8 @@ for module in $(echo "${modules}" | jq -r '.[] | @base64'); do
         echo $message | grep -q "continuous-release-template" && continue
         echo $message | grep -q "exo-release" && continue
         echo $message | grep -q "parent-pom" && continue
+        echo $message | grep -q "eXo Tasks notifications" && continue
+        echo $message | grep -q "Specify base branch when merging PR for eXo Tasks notifications" && continue
         #echo $message | grep -q "Merge Translation" && continue
         author=$(git show --format="%an" -s $commitId)
         commitLink="$modulelink/commit/$(git rev-parse $commitId)"
