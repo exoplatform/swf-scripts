c13bc9f0d858080b7c5676451d3fb021c9ee66d9
Houssem Ben Ali <hbenali@exoplatform.com>
2022-06-13 18:59:40 +0200

    Add builders transformation task to link

--- github/changelogtribe.sh
+++ github/changelogtribe.sh
@@ -62,7 +62,13 @@ for module in $(echo "${modules}" | jq -r '.[] | @base64'); do
         author=$(git show --format="%an" -s $commitId | sed 's/exo-swf/eXo Software Factory/g' | xargs)
         commitLink="$modulelink/commit/$(git rev-parse $commitId)"
         fomattedCommitId=$(echo $commitId | head -c 7)
-        elt=$(echo "<li>(<a href=\"$commitLink\">$fomattedCommitId</a>) $message <b>$author</b></li>\n\t" | gawk '{ gsub(/"/,"\\\"") } 1')
+        buildersTasks=$(echo  $message | grep -oPi BUILDERS-[0-9]+ | xargs)
+        transormedMessage="$message"
+        for buildersTask in $buildersTasks; do 
+          buildersTaskID=$(echo $buildersTask | sed 's/BUILDERS-//gi')
+          transormedTitle=$(echo $transormedMessage | sed "s|$buildersTask|<a href=\"https://builders.meeds.io/portal/meeds/tasks/taskDetail/$buildersTaskID\">$buildersTask</a>|g")
+        done
+        elt=$(echo "<li>(<a href=\"$commitLink\">$fomattedCommitId</a>) $transormedMessage <b>$author</b></li>\n\t" | gawk '{ gsub(/"/,"\\\"") } 1')
         echo "$commitLink $message *** $author"
         echo "	($fomattedCommitId) $message --- $author" >> $changelogfile
         subbody="$subbody$elt"
