18bb5bc2076ab71d5dea8d4561a84253cae36402
Houssem Ben Ali <hbenali@exoplatform.com>
2022-12-12 11:16:27 +0100

    Backport smart cleanup + add tenant provisioning (#93)
    * Backport smart cleanup + add tenant provisioning
    
    * Add meeds private releases to cleanup list

--- nexus/cleanup_meed.sh
+++ nexus/cleanup_meed.sh
@@ -14,11 +14,36 @@ do_delete_curl() {
     curl -u $NEXUS_ADMIN:$NEXUS_PASSWORD -X "DELETE" -w "%{http_code}" "$NEXUS_URL/$1"
 }
 
+do_empty_trash() {
+    do_delete_curl "service/local/wastebasket"
+}
+
+do_delete_artifact_version() {
+    module_name="$1"
+    version_preffix="$2"
+    version_suffix="$3"
+    storage_dir="$4"
+    artifact_dir="$5"
+    echo "$module_name:$version_preffix-$version_suffix"
+    find "$storage_dir/$artifact_dir" -type d -name $version_preffix-$version_suffix | while read dirname; do
+        relativePath=$(echo $dirname | sed "s|$storage_dir/$artifact_dir|$storage_dir/./$artifact_dir|g")
+        relativeAttributesPath=$(echo $dirname | sed "s|$storage_dir/$artifact_dir|$storage_dir/$NEXUS_ATTRIBUTES_FOLDER/./$artifact_dir|g")
+        attributesPath=$(echo $dirname | sed "s|$storage_dir/$artifact_dir|$storage_dir/$NEXUS_ATTRIBUTES_FOLDER/$artifact_dir|g")
+        rsync -avPR "$relativePath" $storage_dir/$NEXUS_TRASH_FOLDER/
+        rsync -avPR "$relativeAttributesPath" $storage_dir/$NEXUS_TRASH_FOLDER/$NEXUS_ATTRIBUTES_FOLDER/
+        rm -rvf $dirname
+        rm -rvf $attributesPath
+    done
+    chown 200:200 -R $storage_dir/$NEXUS_TRASH_FOLDER/$NEXUS_ATTRIBUTES_FOLDER/
+}
+
 NB_RELEASES_TO_KEEP=0 # Nothing in month
-CURRENT_MONTH="10"
+CURRENT_MONTH="11"
 CURRENT_YEAR=2022
 BASE_PATH=/srv/nexus/storage
 BASE_PATH_HOSTED=$BASE_PATH/hosted
+NEXUS_ATTRIBUTES_FOLDER=".nexus/attributes"
+NEXUS_TRASH_FOLDER=".nexus/trash"
 
 ######################
 ##Modules
@@ -50,9 +75,10 @@ NOTES=1.2.0-meed
 # Non-product modules
 DEEDS_DAPP=1.0.0-meed
 DEEDS_TENANT=1.0.0-meed
+DEEDS_TENANT_PROVISIONING=1.0.0-meed
 #####
 # Rest API Nexus repositories list
-NEXUS_REPOSITORIES_LIST="exo-addons-releases exo-releases meeds-releases"
+NEXUS_REPOSITORIES_LIST="exo-addons-releases exo-releases meeds-releases meeds-private-releases"
 
 # Fetch releases 
 releases=($(cat "$BASE_PATH_HOSTED/exo-releases/org/exoplatform/maven-depmgt-pom/maven-metadata.xml" | grep -oP $MAVEN_DEPMGT_POM-${CURRENT_YEAR}${CURRENT_MONTH}[0-9][0-9] | sort --version-sort | uniq | xargs))
@@ -72,58 +98,40 @@ for release in ${releases_to_be_dropped[@]}; do
     echo "==========================================================================================="
     echo " ($counter/${#releases_to_be_dropped[@]}) Dropping Release:  $MEEDS-$rel_suffix..."
     echo "==========================================================================================="
-    echo "maven-depmgt-pom:$MAVEN_DEPMGT_POM-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-releases/org/exoplatform/maven-depmgt-pom -type d -name $MAVEN_DEPMGT_POM-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "gatein-wci:$GATEIN_WCI-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-releases/org/exoplatform/gatein/wci -type d -name $GATEIN_WCI-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "kernel:$KERNEL-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-releases/org/exoplatform/kernel -type d -name $KERNEL-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "core:$CORE-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-releases/org/exoplatform/core -type d -name $CORE-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "ws:$WS-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-releases/org/exoplatform/ws -type d -name $WS-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "gatein-pc:$GATEIN_PC-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-releases/org/exoplatform/gatein/pc -type d -name $GATEIN_PC-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "gatein-sso:$GATEIN_SSO-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-releases/org/exoplatform/gatein/sso -type d -name $GATEIN_SSO-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "gatein-portal:$GATEIN_PORTAL-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-releases/org/exoplatform/gatein/portal -type d -name $GATEIN_PORTAL-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "platform-ui:$PLATFORM_UI-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-releases/org/exoplatform/platform-ui -type d -name $PLATFORM_UI-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "commons:$COMMONS-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-releases/org/exoplatform/commons -type d -name $COMMONS-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "social:$SOCIAL-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-releases/org/exoplatform/social -type d -name $SOCIAL-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "gamification:$GAMIFICATION-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-addons-releases/org/exoplatform/addons/gamification -type d -name $GAMIFICATION-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "gamification-github:$GAMIFICATION_GITHUB-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-addons-releases/org/exoplatform/addons/gamification-github -type d -name $GAMIFICATION_GITHUB-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "kudos:$KUDOS-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-addons-releases/org/exoplatform/addons/kudos -type d -name $KUDOS-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "perk-store:$PERK_STORE-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-addons-releases/org/exoplatform/addons/perk-store -type d -name $PERK_STORE-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "wallet:$WALLET-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-addons-releases/org/exoplatform/addons/wallet -type d -name $WALLET-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "app-center:$APP_CENTER-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-addons-releases/org/exoplatform/addons/app-center -type d -name $APP_CENTER-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "push-notifications:$PUSH_NOTIFICATIONS-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-addons-releases/org/exoplatform/addons/push-notifications -type d -name $PUSH_NOTIFICATIONS-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "addons-manager:$ADDONS_MANAGER-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-releases/org/exoplatform/platform/addons-manager -type d -name $ADDONS_MANAGER-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "meeds:$MEEDS-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-releases/io/meeds/distribution -type d -name $MEEDS-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "task:$TASK-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-addons-releases/org/exoplatform/addons/task -type d -name $TASK-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "analytics:$ANALYTICS-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-addons-releases/org/exoplatform/addons/analytics -type d -name $ANALYTICS-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "poll:$POLL-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-addons-releases/org/exoplatform/addons/poll -type d -name $POLL-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "notes:$NOTES-$rel_suffix"
-    find $BASE_PATH_HOSTED/exo-releases/org/exoplatform/addons/notes -type d -name $NOTES-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "deeds-dapp:$DEEDS_DAPP-$rel_suffix"
-    find $BASE_PATH/meeds-releases/io/meeds/deeds-dapp -type d -name $DEEDS_DAPP-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
-    echo "deeds-tenant:$DEEDS_TENANT-$rel_suffix"
-    find $BASE_PATH/meeds-releases/io/meeds/deeds-tenant -type d -name $DEEDS_TENANT-$rel_suffix -exec rm -rvf {} \; 2>/dev/null || true
+    # Meeds Core (exo-releases)
+    do_delete_artifact_version "maven-depmgt-pom" $MAVEN_DEPMGT_POM $rel_suffix $BASE_PATH_HOSTED/exo-releases org/exoplatform/maven-depmgt-pom
+    do_delete_artifact_version "kernel" $KERNEL $rel_suffix $BASE_PATH_HOSTED/exo-releases org/exoplatform/kernel
+    do_delete_artifact_version "core" $CORE $rel_suffix $BASE_PATH_HOSTED/exo-releases org/exoplatform/core
+    do_delete_artifact_version "ws" $WS $rel_suffix $BASE_PATH_HOSTED/exo-releases org/exoplatform/ws
+    do_delete_artifact_version "gatein-pc" $GATEIN_PC $rel_suffix $BASE_PATH_HOSTED/exo-releases org/exoplatform/gatein/pc
+    do_delete_artifact_version "gatein-wci" $GATEIN_WCI $rel_suffix $BASE_PATH_HOSTED/exo-releases org/exoplatform/gatein/wci
+    do_delete_artifact_version "gatein-sso" $GATEIN_SSO $rel_suffix $BASE_PATH_HOSTED/exo-releases org/exoplatform/gatein/sso
+    do_delete_artifact_version "gatein-portal" $GATEIN_PORTAL $rel_suffix $BASE_PATH_HOSTED/exo-releases org/exoplatform/gatein/portal
+    do_delete_artifact_version "gatein-portal" $GATEIN_PORTAL $rel_suffix $BASE_PATH_HOSTED/exo-releases org/exoplatform/gatein/portal
+    do_delete_artifact_version "platform-ui" $PLATFORM_UI $rel_suffix $BASE_PATH_HOSTED/exo-releases org/exoplatform/platform-ui
+    do_delete_artifact_version "commons" $COMMONS $rel_suffix $BASE_PATH_HOSTED/exo-releases org/exoplatform/commons
+    do_delete_artifact_version "social" $SOCIAL $rel_suffix $BASE_PATH_HOSTED/exo-releases org/exoplatform/social
+    # Meeds Addons (exo-addons-releases)
+    do_delete_artifact_version "gamification" $GAMIFICATION $rel_suffix $BASE_PATH_HOSTED/exo-addons-releases org/exoplatform/addons/gamification
+    do_delete_artifact_version "gamification-github" $GAMIFICATION_GITHUB $rel_suffix $BASE_PATH_HOSTED/exo-addons-releases org/exoplatform/addons/gamification-github
+    do_delete_artifact_version "kudos" $KUDOS $rel_suffix $BASE_PATH_HOSTED/exo-addons-releases org/exoplatform/addons/kudos
+    do_delete_artifact_version "perk-store" $PERK_STORE $rel_suffix $BASE_PATH_HOSTED/exo-addons-releases org/exoplatform/addons/perk-store
+    do_delete_artifact_version "wallet" $WALLET $rel_suffix $BASE_PATH_HOSTED/exo-addons-releases org/exoplatform/addons/wallet
+    do_delete_artifact_version "app-center" $APP_CENTER $rel_suffix $BASE_PATH_HOSTED/exo-addons-releases org/exoplatform/addons/app-center
+    do_delete_artifact_version "push-notifications" $PUSH_NOTIFICATIONS $rel_suffix $BASE_PATH_HOSTED/exo-addons-releases org/exoplatform/addons/push-notifications
+    do_delete_artifact_version "poll" $POLL $rel_suffix $BASE_PATH_HOSTED/exo-addons-releases org/exoplatform/addons/poll
+    do_delete_artifact_version "task" $TASK $rel_suffix $BASE_PATH_HOSTED/exo-addons-releases org/exoplatform/addons/task
+    do_delete_artifact_version "analytics" $ANALYTICS $rel_suffix $BASE_PATH_HOSTED/exo-addons-releases org/exoplatform/addons/analytics
+    # Meeds Addons (exo-releases)
+    do_delete_artifact_version "notes" $NOTES $rel_suffix $BASE_PATH_HOSTED/exo-releases org/exoplatform/addons/notes
+    do_delete_artifact_version "addons-manager" $ADDONS_MANAGER $rel_suffix $BASE_PATH_HOSTED/exo-releases org/exoplatform/platform/addons-manager
+    # Meeds Packaging (exo-releases)
+    do_delete_artifact_version "meeds" $MEEDS $rel_suffix $BASE_PATH_HOSTED/exo-releases io/meeds/distribution
+    # Meeds Third party (meeds-releases)
+    do_delete_artifact_version "deeds-dapp" $DEEDS_DAPP $rel_suffix $BASE_PATH/meeds-releases io/meeds/deeds-dapp
+    do_delete_artifact_version "deeds-tenant" $DEEDS_TENANT $rel_suffix $BASE_PATH/meeds-releases io/meeds/deeds-tenant
+    # Meeds Private Third party (meeds-private-releases)
+    do_delete_artifact_version "deeds-tenant-provisioning" $DEEDS_TENANT_PROVISIONING $rel_suffix $BASE_PATH/meeds-private-releases io/meeds/deeds-tenant-provisioning    
     ((counter++))
 done
 
@@ -132,3 +140,6 @@ for repository in ${NEXUS_REPOSITORIES_LIST}; do
     do_delete_curl "service/local/metadata/repositories/${repository}/content"
     do_delete_curl "service/local/data_index/repositories/${repository}/content"
 done
+
+# Empty Trash
+do_empty_trash
