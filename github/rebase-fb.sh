52515b5198dee2d33b8a97df5a0cba5a70307b42
Houssem Ben Ali <hbenali@exoplatform.com>
2023-01-11 21:44:00 +0100

    Refactor rebase fb script + display counter for divergence check

--- github/rebase-fb.sh
+++ github/rebase-fb.sh
@@ -12,18 +12,22 @@ echo -e "\033[1;31m[Error]\033[0m $1"
 success() {
 echo -e "\033[1;32m[Success]\033[0m $1"
 }
+
+action() {
+echo -e "\033[1;36m[Action]:\033[0m $1"
+}
 ###
 
 [ -z "${FB_NAME}" ] && exit 1
 info "Parsing FB ${FB_NAME} Seed Job Configuration..."
 export FILTER_BRANCH_SQUELCH_WARNING=1 #filter-branch hide warnings
 current_date=$(date '+%s')
-echo "Parsing FB repositories from catalog..."
+action "Parsing FB repositories from catalog..."
 fblist=$(gh api -H 'Accept: application/vnd.github.v3.raw' "/repos/exoplatform/swf-jenkins-pipeline/contents/dsl-jobs/FB/seed_jobs_FB_$(echo ${FB_NAME//-} | tr '[:upper:]' '[:lower:]').groovy" | grep "project:")
 modules_length=$(echo $fblist | grep -o 'project:' | wc -w)
-echo "Modules count: ${modules_length}"
+info "Modules count: ${modules_length}"
 counter=0
-echo "Done. Performing action..."
+action "Done. Performing action..."
 while IFS=']' read -r line; do
     counter=$((counter+1))  
     item=$(echo $line | awk -F'project:' '{print $2}' | cut -d "," -f 1 | tr -d "'"| xargs)
@@ -42,13 +46,14 @@ while IFS=']' read -r line; do
       baseBranch="${BASE_BRANCH}"
     fi
     [ "${org,,}" = "meeds-io" ] || baseBranch="develop"
-    echo "Checking divergence status between ${baseBranch} and feature/${FB_NAME} on repository: ${org}/${item}..."
+    action "(${counter}/${modules_length}) -- Checking divergence status between ${baseBranch} and feature/${FB_NAME} on repository: ${org}/${item}..."
     status=$(gh api /repos/${org}/${item}/compare/${baseBranch}...feature/${FB_NAME} | jq .status | xargs -r echo)
     if [ "${status:-}" != "diverged" ]; then
       info "Not diverged -> Skipped!"
       continue
     fi
-    info "Divergence detected! Starting rebase..."
+    info "Divergence detected!"
+    action "Starting rebase..."
     git clone git@github.com:${org}/${item}.git &>/dev/null
     pushd $item &>/dev/null
     upstream=$(git log --oneline origin/${baseBranch}..origin/feature/${FB_NAME} | wc -l)
@@ -60,7 +65,8 @@ while IFS=']' read -r line; do
     git checkout feature/${FB_NAME} &>/dev/null
     prev_head=$(git rev-parse --short HEAD)
     if ! git rebase origin/${baseBranch} feature/${FB_NAME} >/dev/null; then
-      info "Rebase with recursive strategy has failed! Trying ours rebase strategy without detecting changes loss (helpful for detecting and removing backported commits)..."
+      info "Rebasing with recursive strategy has failed!"
+      action "Trying ours rebase strategy without detecting changes loss (helpful for detecting and removing backported commits)..."
       git rebase --abort || :
       if ! git rebase origin/${baseBranch} feature/${FB_NAME} --strategy-option ours >/dev/null || [ ! -z "$(git diff -w origin/feature/${FB_NAME})" ]; then 
         error "Could not rebase feature/${FB_NAME}!"
