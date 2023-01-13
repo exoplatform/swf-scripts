b5bf8142a59079c54f290618ac1c48ca3f8d5172
Houssem Ben Ali <hbenali@exoplatform.com>
2023-01-13 15:32:44 +0100

    Refactor code

--- github/unlock-fb-protection.sh
+++ github/unlock-fb-protection.sh
@@ -5,24 +5,12 @@
 ##
 # Determines if a value exists in an array.
 ###
-function hasArrayValue ()
-{
+function hasArrayValue() {
     local needle="$1"
-
     shift 1
-
     local haystack="$@"
-
-    # Linear search. Upgrade to binary search for large datasets.
-    for value in "$haystack"; do
-        if [[ $value == $needle ]] ; then
-            return 0
-        fi
-    done
-
-    return 1
+    [[ ${haystack} =~ "${needle}" ]]
 }
-
 
 echo "Parsing FB ${FB_NAME} Seed Job Configuration..."
 
@@ -38,39 +26,48 @@ hash gh 2>/dev/null
 
 current_date=$(date '+%s')
 echo "Parsing FB repositories from catalog..."
-curl -H "Authorization: token ${GIT_TOKEN}" \
-    -H 'Accept: application/vnd.github.v3.raw' \
-    -L "https://api.github.com/repos/exoplatform/swf-jenkins-pipeline/contents/dsl-jobs/FB/seed_jobs_FB_${FB_NAME}.groovy" --output fblist.txt
-cat fblist.txt | grep "project:" >fblistfiltred.txt
+fblist=$(gh api -H 'Accept: application/vnd.github.v3.raw' "/repos/exoplatform/swf-jenkins-pipeline/contents/dsl-jobs/FB/seed_jobs_FB_$(echo ${FB_NAME//-/} | tr '[:upper:]' '[:lower:]').groovy" | grep "project:")
+modules_length=$(echo $fblist | grep -o 'project:' | wc -w)
+echo "Modules count: ${modules_length}"
 
-if [ -z "${MODULES}" ]; then 
-  echo "No modules specified!"
-  exit 1
+if [ -z "${MODULES}" ]; then
+    echo "No modules specified!"
+    exit 1
 fi
-
-echo "Done. Performing action..."
+echo "Done. Checking modules..."
 MODULES="$(echo ${MODULES} | sed 's/,/ /g')"
-while IFS= read -r line; do
+MODULES_LENGTH="$(echo ${MODULES} | wc -w)"
+# Check phase
+counter=0
+while IFS=']' read -r line; do
     item=$(echo $line | awk -F'project:' '{print $2}' | cut -d "," -f 1 | tr -d "'" | xargs)
     org=$(echo $line | awk -F'gitOrganization:' '{print $2}' | cut -d "," -f 1 | tr -d "'" | tr -d "]" | xargs)
     [ -z "${item}" ] && continue
     [ -z "${org}" ] && continue
-    [ $org = "juzu" ] && continue
-    hasArrayValue $item ${MODULES} || continue
-    echo "Unlocking protection on $org/$item:feature/${FB_NAME}"
+    hasArrayValue $item ${MODULES} && counter=$((counter + 1)) || continue
+done <<<"$fblist"
+if [ "${counter}" -ne "${MODULES_LENGTH}" ]; then
+    echo "Error Checks failed! Abort"
+    exit 1
+fi
+echo "Checks Done. Performing unlock protection..."
+for module in $MODULES; do
+    echo "Unlocking protection on $module:feature/${FB_NAME}"
     set +e
-    curl -f -XDELETE -L "https://api.github.com/repos/$org/$item/branches/feature/${FB_NAME}/protection" \
-        --header 'Accept: application/vnd.github.luke-cage-preview+json' \
-        --header "Authorization: Bearer ${GIT_TOKEN}"
+    gh api --method DELETE -H 'Accept: application/vnd.github.luke-cage-preview+json' "/repos/$module/branches/feature/${FB_NAME}/protection"
     set -e
-    echo "OK. You have ${DELAY} minutes to perform actions. Please hurry up ! :xD"
-    echo "Waiting ${DELAY} minutes..."
-    sleep ${DELAY}m
-    echo "Time's up! Restoring branch protection."
-    curl -f -XPUT -L "https://api.github.com/repos/$org/$item/branches/feature/${FB_NAME}/protection" \
+done
+echo "Branches unlocked!"
+echo "OK. You have ${DELAY} minutes to perform actions. Please hurry up ! :xD"
+echo "Waiting ${DELAY} minutes..."
+sleep ${DELAY}m
+echo "Time's up! Restoring branch protection."
+for module in $MODULES; do
+    echo "Restoring protection on $module:feature/${FB_NAME}"
+    curl -f -XPUT -L "https://api.github.com/repos/$module/branches/feature/${FB_NAME}/protection" \
         --header 'Accept: application/vnd.github.luke-cage-preview+json' \
-        --header "Authorization: Bearer ${GIT_TOKEN}" \
+        --header "Authorization: Bearer ${GH_TOKEN}" \
         --header 'Content-Type: application/json' \
         -d '{"required_status_checks":{"strict": true,"contexts": ["exo-ci/build-status"]},"required_pull_request_reviews":{"dismissal_restrictions": {},"dismiss_stale_reviews": true,"require_code_owner_reviews": false,"required_approving_review_count": 1}, "allow_force_pushes":true,"enforce_admins":false,"restrictions":null}' &>/dev/null
-    echo "Branch is now protected!"    
-done <fblistfiltred.txt
+done
+echo "Branches now protected!"
