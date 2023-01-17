f5fc62752fb78fd3e2da1e5cf074646b9ca588a4
Houssem Ben Ali <hbenali@exoplatform.com>
2023-01-17 09:28:12 +0100

    Rebase FB: Remove useless hyperlink

--- github/rebase-fb.sh
+++ github/rebase-fb.sh
@@ -60,7 +60,7 @@ while IFS=']' read -r line; do
     downstream=$(git log --oneline origin/feature/${FB_NAME}..origin/${baseBranch} | wc -l)
     [ "$downstream" -gt "0" ] && downStreamMsg="\033[1;31m${downstream}\033[0m" || downStreamMsg="\033[1;34m${downstream}\033[0m" # if downstream > 1 color red else color blue
     echo "================================================================================================="
-    echo -e " Module (${counter}/${modules_length}): \e]8;;http://github.com/${org}/${item}\a${org}/${item}\e]8;;\a -- Base Branch: ${baseBranch} -- Diff: ^ \033[1;34m${upstream}\033[0m, v ${downStreamMsg}"
+    echo -e " Module (${counter}/${modules_length}): ${org}/${item} -- Base Branch: ${baseBranch} -- Diff: ^ \033[1;34m${upstream}\033[0m, v ${downStreamMsg}"
     echo "================================================================================================="
     git checkout feature/${FB_NAME} &>/dev/null
     prev_head=$(git rev-parse --short HEAD)
