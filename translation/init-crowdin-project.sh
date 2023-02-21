076b0ffc6105cc61ba99c1bffdd97a3a80c1bb79
bourasom <obouras@exoplatform.com>
2023-02-21 16:41:16 +0100

    remove dapp and deeds tenant from stable's translations

--- translation/init-crowdin-project.sh
+++ translation/init-crowdin-project.sh
@@ -38,8 +38,6 @@ AGENDA_CONNECTORS_VERSION=1.1.x
 DLP_VERSION=1.0.x
 ANTI_BRUTEFORCE_VERSION=1.0.x
 ANTI_MALWARE_VERSION=1.0.x
-DEEDS_DAPP_VERSION=1.0.x
-DEEDS_TENANT_VERSION=1.0.x
 CLOUD_DRIVE_CONNECTORS_VERSION=1.0.x
 
 CROWDIN_PROJECT_NAME=$1
@@ -73,7 +71,7 @@ function initTranslation() {
 set -e
 mkdir -p sources
 
-projects=("gatein-portal:${PLF_VERSION}" "platform-ui:${PLF_VERSION}" "commons:${PLF_VERSION}" "ecms:${PLF_VERSION}" "social:${PLF_VERSION}" "notes:${NOTES_VERSION}" "analytics:${ANALYTICS_VERSION}" "meeds:${MEEDS_VERSION}" "platform-private-distributions:${PLF_VERSION}" "chat-application:${CHAT_ADDON_VERSION}" "push-notifications:${PUSH_NOTIFICATIONS_ADDON_VERSION}" "task:${TASK_ADDON_VERSION}" "web-conferencing:${WEB_CONFERENCING_ADDON_VERSION}" "wallet:${WALLET_ADDON_VERSION}" "kudos:${KUDOS_ADDON_VERSION}" "perk-store:${PERKSTORE_ADDON_VERSION}" "gamification:${GAMIFICATION_VERSION}" "news:${NEWS_VERSION}" "onlyoffice:${ONLYOFFICE_VERSION}" "app-center:${APPCENTER_VERSION}" "digital-workplace:${DIGITAL_WORKPLACE_VERSION}" "agenda:${AGENDA_VERSION}" "jitsi:${JITSI_VERSION}" "poll:${POLL_VERSION}" "gamification-github:${GAMIFICATION_GITHUB_VERSION}" "mail-integration:${MAIL_INTEGRATION_VERSION}" "multifactor-authentication:${MULTIFACTOR_AUTHENTICATION_VERSION}" "automatic-translation:${AUTOMATIC_TRANSLATION_VERSION}" "documents:${DOCUMENTS_VERSION}" "processes:${PROCESSES_VERSION}" "agenda-connectors:${AGENDA_CONNECTORS_VERSION}" "cloud-drive-connectors:${CLOUD_DRIVE_CONNECTORS_VERSION}" "deeds-tenant:${DEEDS_TENANT_VERSION}" "deeds-dapp:${DEEDS_DAPP_VERSION}" "anti-malware:${ANTI_MALWARE_VERSION}" "anti-bruteforce:${ANTI_BRUTEFORCE_VERSION}" "dlp:${DLP_VERSION}") 
+projects=("gatein-portal:${PLF_VERSION}" "platform-ui:${PLF_VERSION}" "commons:${PLF_VERSION}" "ecms:${PLF_VERSION}" "social:${PLF_VERSION}" "notes:${NOTES_VERSION}" "analytics:${ANALYTICS_VERSION}" "meeds:${MEEDS_VERSION}" "platform-private-distributions:${PLF_VERSION}" "chat-application:${CHAT_ADDON_VERSION}" "push-notifications:${PUSH_NOTIFICATIONS_ADDON_VERSION}" "task:${TASK_ADDON_VERSION}" "web-conferencing:${WEB_CONFERENCING_ADDON_VERSION}" "wallet:${WALLET_ADDON_VERSION}" "kudos:${KUDOS_ADDON_VERSION}" "perk-store:${PERKSTORE_ADDON_VERSION}" "gamification:${GAMIFICATION_VERSION}" "news:${NEWS_VERSION}" "onlyoffice:${ONLYOFFICE_VERSION}" "app-center:${APPCENTER_VERSION}" "digital-workplace:${DIGITAL_WORKPLACE_VERSION}" "agenda:${AGENDA_VERSION}" "jitsi:${JITSI_VERSION}" "poll:${POLL_VERSION}" "gamification-github:${GAMIFICATION_GITHUB_VERSION}" "mail-integration:${MAIL_INTEGRATION_VERSION}" "multifactor-authentication:${MULTIFACTOR_AUTHENTICATION_VERSION}" "automatic-translation:${AUTOMATIC_TRANSLATION_VERSION}" "documents:${DOCUMENTS_VERSION}" "processes:${PROCESSES_VERSION}" "agenda-connectors:${AGENDA_CONNECTORS_VERSION}" "cloud-drive-connectors:${CLOUD_DRIVE_CONNECTORS_VERSION}" "anti-malware:${ANTI_MALWARE_VERSION}" "anti-bruteforce:${ANTI_BRUTEFORCE_VERSION}" "dlp:${DLP_VERSION}") 
 
 for projectWithVersion in "${projects[@]}"
 do
