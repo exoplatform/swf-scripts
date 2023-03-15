ea9b316c133cbb2e1b9523a751dea76d59f346e2
Houssem Ben Ali <hbenali@exoplatform.com>
2023-03-15 10:29:55 +0100

    Add Storage CI/CD releases cleanup Jenkins script (#108)

---
+++ releases/cleanup_storage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eu
+# This Jenkins job script to clean up old CI/CD releases on storage server
+
+if [ -z "${RELEASES_DIR:-}" ]; then 
+  echo "Error! This job requires RELEASES_DIR variable to be set! Abort"
+  exit 1
+fi
+
+currentmonth=$(date +%Y-%m )
+CURRENT_MONTH=$(date -d "$currentmonth-15 last month" '+%m') # Previous month -> OK with trailing 0 for digits lower than 10
+CURRENT_YEAR=$(date -d "$currentmonth-15 last month" '+%Y') # Previous year if month = 1
+CICD_SUFFIX=${CURRENT_YEAR}${CURRENT_MONTH}
+
+echo "Cleaning up release with suffix containing: ${CICD_SUFFIX}..."
+find ${RELEASES_DIR} -name "*-${CICD_SUFFIX}*" -type d -exec rm -rvf \;
+echo "Cleanup done."
