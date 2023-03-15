be66fa635c0b37e17146f22248f350c4bc665565
Houssem Ben Ali <hbenali@exoplatform.com>
2023-03-15 14:22:05 +0100

    Storage releases cleanup: use webdav API instead of bash (#109)

--- releases/cleanup_storage.sh
+++ releases/cleanup_storage.sh
@@ -6,11 +6,22 @@ if [ -z "${RELEASES_DIR:-}" ]; then
   exit 1
 fi
 
+if [ -z "${RELEASES_URL:-}" ]; then 
+  echo "Error! This job requires RELEASES_URL variable to be set! Abort"
+  exit 1
+fi
+
+if [ -z "${STORAGE_CRED:-}" ]; then 
+  echo "Error! This job requires STORAGE_CRED secret variable to be set! Abort"
+  exit 1
+fi
+
 currentmonth=$(date +%Y-%m )
 CURRENT_MONTH=$(date -d "$currentmonth-15 last month" '+%m') # Previous month -> OK with trailing 0 for digits lower than 10
 CURRENT_YEAR=$(date -d "$currentmonth-15 last month" '+%Y') # Previous year if month = 1
 CICD_SUFFIX=${CURRENT_YEAR}${CURRENT_MONTH}
 
 echo "Cleaning up release with suffix containing: ${CICD_SUFFIX}..."
-find ${RELEASES_DIR} -name "*-${CICD_SUFFIX}*" -type d | xargs -r rm -rvf
+pushd ${RELEASES_DIR}
+find -name "*-${CICD_SUFFIX}*" -type d -printf '%P\n' | xargs -r -L 1 -I {} curl -H "Authorization: Basic ${STORAGE_CRED}" -XDELETE  "${RELEASES_URL}/private/releases/{}/"
 echo "Cleanup done."
