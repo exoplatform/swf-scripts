886d32e82200a19a061083b3f3392120154dd327
Houssem Ben Ali <hbenali.tn@gmail.com>
2023-04-25 11:20:04 +0200

    Add PR comment + refactor fetching

--- github/close_opened_prs.sh
+++ github/close_opened_prs.sh
@@ -1,29 +1,22 @@
 #!/bin/bash -eu
 # Requires MAX_DAYS_TOKEEP : max days number to keep pr opened.
 
-convert2json() {
-    echo "["$(cat /tmp/prlist.txt | grep -o -P '(?<=\[).*(?=, privacy:)' | sed -e "s/$/ },/" -e "s/^/{ /g" -e 's/project/"project"/g' -e 's/gitOrganization/"gitOrganization"/g') "]" | sed -e "s/'/\"/g" -e "s/, ]/]/g" | jq -r '.[] | @base64'
-}
-
 echo "Parsing PR Seed Job Configuration..."
 
 current_date=$(date '+%s')
-echo "Parsing releases branch from catalog..."
-rm -f /tmp/prlist.txt &>/dev/null
-curl -H "Authorization: token ${GIT_TOKEN}" \
-    -H 'Accept: application/vnd.github.v3.raw' \
-    -L "https://api.github.com/repos/exoplatform/swf-jenkins-pipeline/contents/dsl-jobs/PR/seed_jobs_pr.groovy" --output /tmp/prlist.txt
+prModules=$(gh api -H 'Accept: application/vnd.github.v3.raw' "/repos/exoplatform/swf-jenkins-pipeline/contents/dsl-jobs/PR/seed_jobs_pr.groovy" | grep "project:")
 
-items=$(convert2json)
-
+modules_length=$(echo $prModules | grep -o 'project:' | wc -w)
+echo "Modules count: ${modules_length}"
+counter=0
 echo "Done. Performing action..."
-for row in ${items}; do
-    _jq() {
-        echo ${row} | base64 --decode | jq -r ${1}
-    }
-    item="$(_jq '.project')"
-    org="$(_jq '.gitOrganization')"
-    echo "Fetching open pull request of $org/$item...."
+while IFS=']' read -r line; do
+    counter=$((counter+1))  
+    item=$(echo $line | awk -F'project:' '{print $2}' | cut -d "," -f 1 | tr -d "'"| xargs)
+    org=$(echo $line | awk -F'gitOrganization:' '{print $2}' | cut -d "," -f 1 | tr -d "'" | tr -d "]"| xargs)
+    [ -z "${item}" ] && continue
+    [ -z "${org}" ] && continue
+    echo "(${counter}/${modules_length}) Fetching open pull request of $org/$item...."
     prs=$(gh api -X GET "/repos/$org/$item/pulls?state=open" | jq -r '.[] | @base64')
     echo "Done."
     for pr in ${prs}; do
@@ -40,10 +33,11 @@ for row in ${items}; do
         diff_in_days=$(((current_date - epoch_updated_date) / (60 * 60 * 24)))
         if [ $diff_in_days -gt $MAX_DAYS_TOKEEP ]; then
             gh api -X DELETE "/repos/$org/$item/git/refs/heads/${ref}"
+            gh api -X POST "/repos/$org/$item/issues/$number/comments" -f body='Stale PR. Closed' 
             echo "PR: $org/$item/$number: \"${title}\" has been closed. Latest update date: $updated_at"
         else
             echo "PR: $org/$item/$number: \"${title}\" is Kept. Latest update date: $(date -d $updated_at '+%Y-%m-%d %H:%M:%S'). Stale Remaining days: $((MAX_DAYS_TOKEEP - diff_in_days))"
         fi
     done
     echo "Done fetching prs of $org/$item."
-done
+done <<< "$prModules"
