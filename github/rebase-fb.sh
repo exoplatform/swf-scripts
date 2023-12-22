595e554c535b1f1a06a4e480ee601ca2a38bbee0
Houssem Ben Ali <hbenali.tn@gmail.com>
2023-12-22 10:11:55 +0100

    Rebase FB: Fix company modules fetch

--- github/rebase-fb.sh
+++ github/rebase-fb.sh
@@ -32,7 +32,7 @@ action "Parsing FB repositories from catalog..."
 fblist=$(gh api -H 'Accept: application/vnd.github.v3.raw' "/repos/exoplatform/swf-jenkins-pipeline/contents/dsl-jobs/FB/seed_jobs_FB_$(echo ${FB_NAME//-} | tr '[:upper:]' '[:lower:]').groovy" | grep "project:")
 if ${COMPANY_MODULES_ENABLED:-false}; then 
   cmpfblist=$(gh api -H 'Accept: application/vnd.github.v3.raw' "/repos/exoplatform/swf-jenkins-pipeline/contents/dsl-jobs/company/seed_jobs_ci.groovy" | grep "feature/${FB_NAME}" | grep "project:")
-  fblist=$(echo -e "${fblist}\n${cmpfblist}")
+  [ -z "${cmpfblist:-}"] || fblist=$(echo -e "${fblist}\n${cmpfblist}")
 fi
 
 modules_length=$(echo $fblist | grep -o 'project:' | wc -w)
