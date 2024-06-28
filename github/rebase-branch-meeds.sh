ed012d41c41653c6104fe3f324c3877fa4166672
Houssem Ben Ali <hbenali.tn@gmail.com>
2024-06-28 10:45:11 +0200

    Tmp: Rename parent poms groupIds: Prevent rebasing parent modules

--- github/rebase-branch-meeds.sh
+++ github/rebase-branch-meeds.sh
@@ -30,6 +30,7 @@ while IFS=']' read -r line; do
     org=$(echo $line | awk -F'gitOrganization:' '{print $2}' | cut -d "," -f 1 | tr -d "'" | tr -d "]"| xargs)
     [ -z "${item}" ] && continue
     [ -z "${org}" ] && org=${DEFAULT_ORG}
+    [[ "${item}" =~ -parent-pom$ ]] && continue
     echo "================================================================================================="
     echo -e " Module (${counter}/${modules_length}): \e]8;;http://github.com/${org}/${item}\a${org}/${item}\e]8;;\a"
     echo "================================================================================================="
