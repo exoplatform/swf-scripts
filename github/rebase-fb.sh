56a387e9485d9254f40d73fb0db757db06c5ffdb
Houssem Ben Ali <hbenali.tn@gmail.com>
2025-03-19 16:11:27 +0100

    Recreate FB mode

--- github/rebase-fb.sh
+++ github/rebase-fb.sh
@@ -138,36 +138,6 @@ while IFS=']' read -r line; do
     
     [ "${org,,}" = "meeds-io" ] || baseBranch="develop"
         
-    # Check the comparison status between branches
-    compareJson=$(gh api /repos/${org}/${item}/compare/${baseBranch}...feature/${FB_NAME})
-    status=$(echo "$compareJson" | jq -r .status)
-    aheadby=$(echo "$compareJson" | jq -r .ahead_by)
-    behindby=$(echo "$compareJson" | jq -r .behind_by)
-    if [ "${behindby}" -eq "0" ]; then 
-      behindbyMsg="\033[1;32m${behindby}\033[0m"
-    else 
-      behindbyMsg="\033[1;31m${behindby}\033[0m"
-    fi
-    aheadbyMsg="\033[1;34m$aheadby\033[0m"
-    formatted_counter=$(printf "%0${counter_width}d" $counter)
-
-    # Calculate status color length without codes
-    colored_status=$(colorize_status "$status")
-    status_length=$(strip_color_codes "$colored_status")
-    status_width=$((status_width > ${#status_length} ? status_width : ${#status_length}))
-
-    # Format the fields for tabular display
-    printf "\033[1;34m[Info]\033[0m %-*s %-*s %-*s %-*s %-*s\n" \
-        $((counter_width + 3)) "(${formatted_counter}/${modules_length})" \
-        $repo_width "${org}/${item}" \
-        $branch_width "${baseBranch}...feature/${FB_NAME}" \
-        $status_width "${colored_status}" \
-        $ahead_behind_width "Ahead: $(echo -e $aheadbyMsg), Behind: $(echo -e $behindbyMsg)"
-
-    # If not diverged, no rebase is needed
-    if [ "${status:-}" != "diverged" ]; then
-        continue
-    fi
 
     modulesToRebase="${modulesToRebase} ${org}@${item}@${baseBranch}"
 done <<< "$fblist"
@@ -177,7 +147,7 @@ if [ -z "${modulesToRebase}" ]; then
   exit 0
 fi
 
-action "Starting rebase repositories..."
+action "Starting creating FB..."
 
 # Rebase each module in the list
 rebasesCounter=0
@@ -186,85 +156,17 @@ for moduleToRebase in ${modulesToRebase}; do
     org=$(echo $moduleToRebase | cut -d '@' -f1)
     item=$(echo $moduleToRebase | cut -d '@' -f2 )
     baseBranch=$(echo $moduleToRebase | cut -d '@' -f3)
-    action "Rebasing branch feature/${FB_NAME} on ${baseBranch} of repository ${org}/${item}..."
+    action "Creating branch feature/${FB_NAME} on ${baseBranch} of repository ${org}/${item}..."
     # Clone repository and rebase feature branch
     git clone git@github.com:${org}/${item}.git &>/dev/null
     pushd $item &>/dev/null
     git checkout feature/${FB_NAME} &>/dev/null
     prev_head=$(git rev-parse --short HEAD)
-
-    # Attempt rebase with recursive strategy, fallback to 'ours' if it fails
-    if ! git -c advice.skippedCherryPicks=false rebase origin/${baseBranch} feature/${FB_NAME} >/dev/null; then
-        info "Rebasing with recursive strategy has failed!"
-        action "Trying 'ours' rebase strategy..."
-        git rebase --abort || :
-        
-        if ! git -c advice.skippedCherryPicks=false rebase origin/${baseBranch} feature/${FB_NAME} --strategy-option ours >/dev/null || [ ! -z "$(git diff -w origin/feature/${FB_NAME})" ]; then 
-            error "Could not rebase feature/${FB_NAME} for ${org}/${item}!"
-            exit 1
-        fi
-    fi
-
-    # Log changes before and after the rebase
-    git log --oneline --cherry origin/${baseBranch}..HEAD
-    if [ ! -z "$(git diff origin/feature/${FB_NAME} 2>/dev/null)" ]; then
-        info "Changes before the rebase:"
-        echo -e "\033[1;32m****\033[0m"
-        git log HEAD..origin/feature/${FB_NAME} --oneline --pretty=format:"(%C(yellow)%h%Creset) %s"
-        echo -e "\033[1;32m****\033[0m"
-    else 
-        info "No changes detected!"  
-    fi
-
-    new_head=$(git rev-parse --short HEAD)
-    
-    # If the HEAD has changed, push and handle Jenkins job cancellation if needed
-    if [ "${prev_head}" != "${new_head}" ]; then
-        info "Previous HEAD: \033[1;31m${prev_head}\033[0m, New HEAD: \033[1;32m${new_head}\033[0m."
-        git push origin feature/${FB_NAME} --force-with-lease 2>&1 | grep -v remote ||:
-        
-        # Cancel Jenkins job if required
-        if [ ! -z "${JENKINS_HOST:-}" ] && [ "${rebasesCounter}" -gt "1" ]; then 
-            cancelJenkinsQueuedJobName "${item}-${FB_NAME}-fb-ci" &  # Async call
-        fi
-
-        # Check for incorrect submodule versions
-        wrongSubmodulesVersion=$(grep -Pirl '<version>.*\.x(-exo|-meed)?-SNAPSHOT</version>' --include=pom.xml | xargs)
-        if [ ! -z ${wrongSubmodulesVersion} ]; then
-            warn "Unfixed ${FB_NAME} FB Maven submodules version detected!"
-            grep -Pirn '<version>.*\.x(-exo|-meed)?-SNAPSHOT</version>' --include=pom.xml | sed -E 's/^/ - /g'
-        fi
-
-        # Handle PR rebases if REBASE_PRS is true
-        if ${REBASE_PRS}; then 
-            action "Looking for PRs with base feature/${FB_NAME}..."
-            fbPRs=$(gh api /repos/${org}/${item}/pulls?base=feature/${FB_NAME})
-            
-            for fbPR in $(echo "${fbPRs}" | jq -r '.[] | @base64'); do
-                _jq() {
-                    echo ${fbPR} | base64 --decode | jq -r ${1}
-                }
-                prBranch=$(_jq '.head.ref')
-                prNumber=$(_jq '.number')
-                action "Trying to rebase PR https://github.com/${org}/${item}/pull/${prNumber} branch: ${prBranch}..."
-                
-                # Rebase PR branch
-                git checkout -f ${prBranch} &>/dev/null
-                prev_head=$(git rev-parse --short HEAD)
-                
-                if ! git rebase feature/${FB_NAME} ${prBranch} >/dev/null; then 
-                    error "Cannot rebase ${prBranch} on feature/${FB_NAME} for ${org}/${item}! Skipped!"
-                    git rebase --abort &>/dev/null || :
-                else
-                    new_head=$(git rev-parse --short HEAD)
-                    info "Previous HEAD: \033[1;31m${prev_head}\033[0m, New HEAD: \033[1;32m${new_head}\033[0m."
-                    git push origin ${prBranch}:${prBranch} --force-with-lease 2>&1 | grep -v remote ||:
-                fi
-            done
-            info "PRs rebase finished."
-        fi
-    fi
-
+    git reset --hard origin/${baseBranch} &>/dev/null
+    sed -Ei "s#<version>28(-exo|-meed)?-(M[0-9][0-9]|SNAPSHOT)</version>#<version>28-${FB_NAME}-SNAPSHOT</version>#g" pom.xml
+    sed -Ei "s#<version>19(-exo|-meed)?-(M[0-9][0-9]|SNAPSHOT)</version>#<version>19-${FB_NAME}-SNAPSHOT</version>#g" pom.xml
+    find -name pom.xml | xargs sed -Ei "s#\.x(-exo|-meed)?-SNAPSHOT#.x-${FB_NAME}-SNAPSHOT#g"
+    git add -u && git commit -m "TASK-77677: Create FB ${FB_NAME} and update projects versions/dependencies" && git push origin feature/${FB_NAME} --force &>/dev/null || :
     popd &>/dev/null
 done
 
